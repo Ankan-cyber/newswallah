b9b80a60debd5e42cceb8488323348e47d9d1236
Ankan <ankanroy7454@gmail.com>
2023-01-01 16:42:09 +0530

    Initial commit

--- src/components/NewsCard.js
+++ src/components/NewsCard.js
@@ -10,7 +10,7 @@ export default class Newsbody extends Component {
                         <div className="card-body">
                             <h5 className="card-title">{title}</h5>
                             <p className="card-text">{abstract}</p>
-                            <a href={newsUrl} className="btn btn-primary btn-sm" target="_blank" rel='norefer'>Read More</a>
+                            <a href={newsUrl} className="btn btn-primary btn-sm" target="_blank" rel='noreferrer'>Read More</a>
                         </div>
             
                 </div>
