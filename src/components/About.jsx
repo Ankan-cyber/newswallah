70bd929d923f8402aa3bee9c2d0fe870a4b5dd88
Ankan <ankanroy7454@gmail.com>
2023-01-06 22:42:40 +0530

    Completed First Part

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -4,9 +4,32 @@ export default class About extends Component {
     render() {
         return (
             <>
-             {document.title="News Wallah - About Us"}
-                <div className="container my-3" style={{ padding: "4rem" }}>
-                    About
+                <div className="container" style={{padding: "5rem"}}>
+                    <div className="row">
+                        <div className="col-4 text-center">
+                            <img src="/logo.png" className="rounded" alt="logo" />
+                        </div>
+                        <div className="col-8">
+                            <h1>About Us</h1>
+                            <p>
+                                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin
+                                ornare magna eros, eu pellentesque tortor vestibulum ut. Maecenas
+                                non massa sem. Etiam finibus odio quis feugiat facilisis.
+                            </p>
+                            <p>
+                                Curabitur mollis bibendum luctus. Nulla maximus cursus nisl, non
+                                vestibulum urna placerat non. Curabitur auctor nisi quis nisl
+                                tempus, id dictum erat ornare.
+                            </p>
+                            <p>
+                                Vivamus a diam fermentum, luctus ligula sit amet, aliquam risus.
+                                Integer non turpis auctor, bibendum tortor a, interdum neque.
+                                Aliquam erat volutpat. Aliquam bibendum, nisi eu blandit
+                                malesuada, velit nisi dapibus justo, eu consequat justo augue a
+                                neque.
+                            </p>
+                        </div>
+                    </div>
                 </div>
             </>
         )
