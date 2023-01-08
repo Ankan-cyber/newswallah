de3abf8d2c29e8fb1eeffcfefe7bcb01656c2caf
Ankan <ankanroy7454@gmail.com>
2023-01-08 18:15:31 +0530

    Completed

--- src/components/NotFound.jsx
+++ src/components/NotFound.jsx
@@ -8,15 +8,15 @@ export default class NotFound extends Component {
                 {document.title = "404 Not Found"}
                 <section className="notFound">
                     <div className="img">
-                        <a href="#"><img src="https://assets.codepen.io/5647096/backToTheHomepage.png" alt="Back to the Homepage" /></a>
-                        <a href="#"><img src="https://assets.codepen.io/5647096/Delorean.png" alt="El Delorean, El Doc y Marti McFly" /></a>
+                        <img src="https://assets.codepen.io/5647096/backToTheHomepage.png" alt="Back to the Homepage" />
+                        <img src="https://assets.codepen.io/5647096/Delorean.png" alt="El Delorean, El Doc y Marti McFly" />
                     </div>
                     <div className="text">
                         <h1>404</h1>
                         <h2>PAGE NOT FOUND</h2>
                         <h3>BACK TO HOME?</h3>
                         <a href="/" className="yes">YES</a>
-                        <a href="#">NO</a>
+                        <a href="/about">NO</a>
                     </div>
                 </section>
             </>
